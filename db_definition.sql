bde052ac010799c9c7ac8fe52efeb2346153c8f9
Lightning <lightning1575@gmail.com>
2025-03-20 10:52:17 +0700

    saved

---
+++ db_definition.sql
@@ -0,0 +1,106 @@
+CREATE TABLE Users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    address VARCHAR(50) NOT NULL,
+    phone CHAR(10) NOT NULL,
+    role VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Products (
+    product_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    price MONEY NOT NULL,
+    description TEXT NOT NULL,
+    stock INT NOT NULL
+);
+
+CREATE TABLE Orders (
+    order_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    total MONEY NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Order_items(
+    order_item_id SERIAL PRIMARY KEY,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    price MONEY NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
+
+CREATE TABLE Categories(
+    category_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE Product_Categories(
+    product_category_id SERIAL PRIMARY KEY,
+    product_id INT NOT NULL,
+    category_id INT NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES Products(product_id),
+    FOREIGN KEY (category_id) REFERENCES Categories(category_id)
+);
+
+CREATE TABLE Carts(
+    cart_id SERIAL NOT NULL,
+    user_id INT NOT NULL,
+    created_at DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Cart_items(
+    cart_item_id SERIAL NOT NULL,
+    cart_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY (cart_id) REFERENCES Carts(cart_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
+
+CREATE TABLE Payments(
+    payment_id SERIAL PRIMARY KEY,
+    order_id INT NOT NULL,
+    payment_date DATE NOT NULL,
+    payment_method VARCHAR(20) NOT NULL,
+    amount MONEY NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+);
+
+CREATE TABLE Review(
+    review_id SERIAL PRIMARY KEY,
+    product_id INT NOT NULL,
+    user_id INT NOT NULL,
+    rating INT NOT NULL,
+    comment TEXT NOT NULL,
+    created_at DATE NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES Products(product_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Promotion(
+    promotion_id SERIAL PRIMARY KEY,
+    code VARCHAR(20) NOT NULL,
+    discount INT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE Promotion_Apply(
+    usage_id SERIAL PRIMARY KEY,
+    order_id INT NOT NULL,
+    promotion_id INT NOT NULL,
+    used_at DATE NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (promotion_id) REFERENCES Promotion(promotion_id)
+);
